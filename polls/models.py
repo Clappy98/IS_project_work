54e3977a6c13ee969b3542a9fbde09d10d015eb7
Clappy98 <b.raffy.98@gmail.com>
2022-07-04 17:31:35 +0200

    Added database table

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,44 @@
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('Published date')
+class User(models.Model):
+    pass    # Django creates ad autoincrementing ID field as pk
 
-class Choice(models.Model):
+class Question(models.Model):
+    text = models.TextField(primary_key=True)
+    is_negative = models.BooleanField()
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+
+class Performance(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+    year = models.IntegerField()
+    duration = models.IntegerField()
+
+class Background(models.Model):
+    type = models.CharField(max_length=20)
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+
+class QuestionCategory(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
+
+    value = models.IntegerField()
+
+class Experience(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    background = models.ForeignKey(Background, on_delete=models.CASCADE)
+
+class PerformanceCharacteristic(models.Model):
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+
+    value = models.IntegerField()
--- testsite/settings.py
+++ testsite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mod_wsgi.server'
+    #'mod_wsgi.server'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'testsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ispw_db',
+        'USER': 'mydatabaseuser',   # IMPORTANT: FIND THE USERNAME
+        'PASSWORD': 'P455_postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
